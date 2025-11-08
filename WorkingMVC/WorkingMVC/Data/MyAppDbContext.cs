bf8d70695875c9722647f150ff34a351678c6f13
novakvova <novakvova@gmail.com>
2025-11-08 16:44:46 +0200

    Add Identity

--- WorkingMVC/WorkingMVC/Data/MyAppDbContext.cs
+++ WorkingMVC/WorkingMVC/Data/MyAppDbContext.cs
@@ -1,9 +1,14 @@
 ﻿using WorkingMVC.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using WorkingMVC.Data.Entities.Idenity;
+using Microsoft.AspNetCore.Identity;
 
 namespace WorkingMVC.Data;
 
-public class MyAppDbContext : DbContext
+public class MyAppDbContext : IdentityDbContext<UserEntity, RoleEntity, int,
+    IdentityUserClaim<int>, UserRoleEntity, IdentityUserLogin<int>,
+    IdentityRoleClaim<int>, IdentityUserToken<int>>
 {
     public MyAppDbContext(DbContextOptions<MyAppDbContext> dbContextOptions)
         : base(dbContextOptions)
@@ -11,4 +16,20 @@ public class MyAppDbContext : DbContext
 
     //Це таблиця в БД
     public DbSet<CategoryEntity> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        //
+        builder.Entity<UserRoleEntity>()
+            .HasOne(ur => ur.User)
+            .WithMany(u => u.UserRoles)
+            .HasForeignKey(ur => ur.UserId);
+
+        builder.Entity<UserRoleEntity>()
+            .HasOne(ur => ur.Role)
+            .WithMany(u => u.UserRoles)
+            .HasForeignKey(ur => ur.RoleId);
+    }
 }
