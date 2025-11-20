cc84f73792b3debbbab9fc4d5e5268946cb01651
novakvova <novakvova@gmail.com>
2025-11-20 16:59:14 +0200

    Add controller

---
+++ WebApiTransfer/WebApiTransfer/Controllers/CountriesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiTransfer.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CountriesController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetCountries()
+    {
+        
+        // Implementation to retrieve and return countries would go here.
+        return Ok();
+    }
+}
