59425642785dd273721588fb428d608f083b8aa8
novakvova <novakvova@gmail.com>
2025-11-20 16:20:03 +0200

    Working Add Database Connection

--- WebApiTransfer/WebApiTransfer/Program.cs
+++ WebApiTransfer/WebApiTransfer/Program.cs
@@ -1,12 +1,22 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbTransferContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
