912fb4a0866e48da6088fd1ccd6c6cb67415aad4
novakvova <novakvova@gmail.com>
2025-11-29 16:43:43 +0200

    Add Identity Tabels

--- WebApiTransfer/Domain/Migrations/20251129144225_Add Identity Tabels.Designer.cs
+++ WebApiTransfer/Domain/Migrations/20251129144225_Add Identity Tabels.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Domain;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Domain.Migrations
 {
     [DbContext(typeof(AppDbTransferContext))]
-    partial class AppDbTransferContextModelSnapshot : ModelSnapshot
+    [Migration("20251129144225_Add Identity Tabels")]
+    partial class AddIdentityTabels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
