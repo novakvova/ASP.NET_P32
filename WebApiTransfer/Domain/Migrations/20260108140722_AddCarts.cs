b57ca87680ee96a352336ccdba28edc5812d8b17
novakvova <novakvova@gmail.com>
2026-01-08 16:13:50 +0200

    Update Database tblCarts

---
+++ WebApiTransfer/Domain/Migrations/20260108140722_AddCarts.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCarts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tblCarts",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    TransportationId = table.Column<int>(type: "integer", nullable: false),
+                    CountTikets = table.Column<short>(type: "smallint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblCarts", x => new { x.TransportationId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_tblCarts_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_tblCarts_tblTransportations_TransportationId",
+                        column: x => x.TransportationId,
+                        principalTable: "tblTransportations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblCarts_UserId",
+                table: "tblCarts",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tblCarts");
+        }
+    }
+}
